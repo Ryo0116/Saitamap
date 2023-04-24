a370fdabf583f108e385e0c3670010d45b33cff9
Ryo0116 <ryo0116kine@gmail.com>
2023-04-25 00:51:46 +0900

    デプロイエラーを再修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,11 +2,4 @@ class UsersController < ApplicationController
   skip_before_action :require_login, only: [:index]
 
   def index; end
-
-  private
-  def require_login
-    unless logged_in?
-      redirect_to login_url
-    end
-  end
 end
