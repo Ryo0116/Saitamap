2cd652518e5540b445ba496624b705047257172f
Ryo0116 <ryo0116kine@gmail.com>
2023-04-24 23:47:58 +0900

    spot_controller.rbの記載

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -1,4 +1,45 @@
 class SpotsController < ApplicationController
+  protect_from_forgery
+  before_action :authenticate_user!, except: [:show ,:index]
+
   def index
+    @spots = Spot.all
   end
+
+  def new
+    @spot = current_user.spots.build
+  end
+
+  def create
+    @spot = current_user.spots.build(spot_params)
+    if @spot.save
+      redirect_to :spots_posts, notice: "保存しました。"
+    else
+      flash[:alert] = "問題が発生しました。"
+      render :new
+    end
+  end
+
+  def show
+    @spot = Spot.find(params[:id])
+  end
+
+  def update
+    new_params = spot_params
+    new_params = spot_params.merge(active: true) if is_ready_spot
+    if @spot.update(new_params)
+      flash[:notice] = "保存しました。"
+    else
+      flash[:alert] = "問題が発生しました。"
+    end
+    redirect_back(fallback_location: request.referer)
+  end
+
+  def destroy
+    @spot = Spot.find(params[:id])
+    @spot.destroy
+    flash[:notice] = "投稿した場所を削除しました"
+    redirect_to :spots_posts
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :spots do
-
-  end
+  resources :spots 
 end
