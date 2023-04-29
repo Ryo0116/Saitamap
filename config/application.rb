a82cfd02b6812405983df7f2ebfc870b3662f277
Ryo0116 <ryo0116kine@gmail.com>
2023-04-30 00:13:22 +0900

    devise日本語化

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module Saitamap
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
     config.generators.template_engine=:slim #slimに変更
+    config.i18n.default_locale = :ja
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
