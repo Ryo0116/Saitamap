1bf4030e4d7b85b9333ac08d15dd712f47f3703a
Ryo0116 <ryo0116kine@gmail.com>
2023-05-11 07:58:48 +0900

    bootstrap導入完了

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,10 @@
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap/dist/js/bootstrap'
+import Rails from "@rails/ujs"
+import Turbolinks from "turbolinks"
+import * as ActiveStorage from "@rails/activestorage"
+import "channels"
+import "bootstrap";
+import "../stylesheets/application.scss";
 
-require('bootstrap')
-
+Rails.start()
+Turbolinks.start()
+ActiveStorage.start()
