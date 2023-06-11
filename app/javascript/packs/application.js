9ac0e0605df362143f811af9e9ed064419a36e27
Ryo0116 <ryo0116kine@gmail.com>
2023-06-12 07:30:37 +0900

    change: rails-ujfを７に変更

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,10 +1,11 @@
+alert("webpacker!!")
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "bootstrap";
 import "../stylesheets/application.scss";
-
+alert("webpacker!!")
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
