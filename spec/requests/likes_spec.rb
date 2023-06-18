7ace61d62cbdf2daabce261cce68c4c5af6296de
Ryo0116 <ryo0116kine@gmail.com>
2023-06-19 06:34:52 +0900

    add: spec/requsts/likes_spec.rbの追加

--- spec/requests/likes_spec.rb
+++ spec/requests/likes_spec.rb
@@ -1,18 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe "Likes", type: :request do
-  describe "GET /create" do
-    it "returns http success" do
-      get "/likes/create"
-      expect(response).to have_http_status(:success)
+  let(:user) { create(:user) }
+  let(:spot_new) { create(:spot, user: user) }
+  let(:like) { create(:like, user: user, spot: spot_new) }
+
+  before do
+    sign_in user
+  end
+
+  describe "SPOT /#create" do
+    it "データの保存に成功する事" do
+      expect do
+        spot spot_likes_path(spot_new.id), xhr: true
+      end.to change { like.count }.by(1)
     end
   end
 
-  describe "GET /destroy" do
-    it "returns http success" do
-      get "/likes/destroy"
-      expect(response).to have_http_status(:success)
+  describe "DELETE #destroy" do
+    let!(:like) { create(:like, user: user, spot: spot_new) }
+
+    it "データの保存に成功する事" do
+      expect do
+        delete spot_likes_path(spot_new.id), xhr: true
+      end.to change { like.count }.by(-1)
     end
   end
-
 end
+
